69308658c7afe106dcb9fade44fc328af54b63d7
Er. Vineet Rajpal <rajpalvineet800@gmail.com>
2026-06-13 12:21:26 +0530

    complete the admin frontenfd

---
+++ admin/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Sidebar from './Components/Sidebar/Sidebar'
+import { Routes , Route } from 'react-router-dom'
+import Add from './Pages/Add/Add'
+import Order from './Pages/Order/Order'
+import List from './Pages/List/List'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <hr/>
+      <div className='app-content'>
+        <Sidebar/>
+        <Routes>
+          <Route path="/add" element ={<Add/>}/>
+          <Route path="/order" element ={<Order/>}/>
+          <Route path="/list" element ={<List/>}/>
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ admin/src/Pages/Add/Add.jsx
@@ -0,0 +1,87 @@
+import React ,{useState} from "react";
+import "./Add.css";
+import { assets } from "../../assets/assets";
+
+const Add = () => {
+    const [image, setimage] = useState(null)
+      const [data, setData] = useState({
+    name: "",
+    description: "",
+    price: "",
+    category: "Salad",
+  });
+   const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+  return (
+    <div className="add">
+      <form className="add-form">
+
+        <div className="add-img-upload">
+          <p>Upload Image</p>
+          <label htmlFor="image">
+            <img src={image?URL.createObjectURL(image):assets.upload_area} alt="" />
+          </label>
+          <input type="file" id="image"  onChange={(e)=>setimage(e.target.files[0])}   hidden required />
+        </div>
+
+        <div className="add-product-name">
+          <p>Product Name</p>
+          <input
+            type="text"
+            name="name"
+            placeholder="Type here"
+            required
+          />
+        </div>
+
+        <div className="add-product-description">
+          <p>Product Description</p>
+          <textarea
+            name="description"
+            rows="6"
+            placeholder="Write content here"
+            required
+          />
+        </div>
+
+        <div className="add-category-price">
+
+          <div className="add-category">
+            <p>Product Category</p>
+            <select name="category">
+              <option value="Salad">Salad</option>
+              <option value="Rolls">Rolls</option>
+              <option value="Deserts">Deserts</option>
+              <option value="Sandwich">Sandwich</option>
+              <option value="Cake">Cake</option>
+              <option value="Pure Veg">Pure Veg</option>
+              <option value="Pasta">Pasta</option>
+              <option value="Noodles">Noodles</option>
+            </select>
+          </div>
+
+          <div className="add-price">
+            <p>Product Price</p>
+            <input
+              type="number"
+              name="price"
+              placeholder="$20"
+              required
+            />
+          </div>
+
+        </div>
+
+        <button type="submit" className="add-btn">
+          ADD
+        </button>
+
+      </form>
+    </div>
+  );
+};
+
+export default Add;
