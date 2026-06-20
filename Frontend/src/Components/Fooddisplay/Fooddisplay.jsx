0eebb2a130fc9fe1b3e2f19a272fa23dc6154768
Er. Vineet Rajpal <rajpalvineet800@gmail.com>
2026-06-20 20:11:52 +0530

    fetch food from database working frontend is able to get food detials from database

--- Frontend/src/Components/Fooddisplay/Fooddisplay.jsx
+++ Frontend/src/Components/Fooddisplay/Fooddisplay.jsx
@@ -1,15 +1,17 @@
+/* eslint-disable no-undef */
 import React, { useContext } from 'react'
 import "./Fooddisplay.css"
 import { StoreContext } from '../../Context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 
-const Fooddisplay = ({ category }) => {
+const Fooddisplay = ({category}) => {
   const { food_list } = useContext(StoreContext)
-
+  console.log(food_list)
   return (
     <div className='food-display' id='food-display'>
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
+
         {food_list
           .filter(item => category === 'All' || category === item.category)
           .map((item, index) => (
